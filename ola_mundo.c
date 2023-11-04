d6f445431454e07697a696ab9cfd43d7f29c1a25
Th4Hawk <whitereaper7777@protonmail.com>
2023-11-03 22:56:08 -0300

    Olá mundo + Variaveis

---
+++ ola_mundo.c
@@ -0,0 +1,11 @@
+// Biblioteca responsavel pela entrada/saida de dados
+#include <stdio.h>
+
+// Função main
+int main ()
+{
+  // Imprime o texto "Texto legal"
+  printf("Texto legal\n");
+  // Indica que o programa foi executado com sucesso
+  return 0;
+}
---
+++ variaveis.c
@@ -0,0 +1,24 @@
+// Biblioteca responsavel por lidar com a entrada/saida de dados
+#include <stdio.h>
+
+// Função main 
+int main ()
+{ 
+  /*
+    int -> Armazena um valor inteiro %d 
+    char -> Armazena um caractere %c
+    float -> Armazena um numero real %f
+    double -> Armazena um numero real, porem com mais precisão %lf
+    void -> Tipo vazio
+    unsigned char -> Armazena um caractere sem sinal %c
+    long int -> Armazena um numero inteiro, porem com dominio estendido %li
+    unsigned int -> Armazena um numero inteiro, porem apenas positivos %u
+    unsigned long int -> Armazena um numero inteiro positivo com dominio estendido %lu
+    shot int -> Armazena um numero inteiro com dominio reduzido %hi
+    unsigned shor int -> Armazena um inteiro positivo com dominio reduzido %hu
+  */ 
+  // Imprime no console o texto "Variaveis"
+  printf("Variaveis");
+  // Indica que o programa foi executado com sucesso
+  return 0;
+}
