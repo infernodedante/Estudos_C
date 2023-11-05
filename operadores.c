79ce88793508db44128ab2afc5701dfb4812894d
Th4Hawk <whitereaper7777@protonmail.com>
2023-11-05 10:32:00 -0300

    Operadores

---
+++ operadores.c
@@ -0,0 +1,45 @@
+// Biblioteca destinada a lidar com entrada/saida de dados
+#include <stdio.h>
+
+// Função principal
+int main() {
+
+  /*
+    Operadores aritméticos
+    + -> Soma
+    - -> Subtração
+    * -> Multiplicação
+    / -> Divisão
+    % -> Resto da divisão
+    ++ -> Incremento
+    -- -> Decremento
+    Obs: É possivel usar o Incremento e o decremento de forma pós-fixada (++a) e
+pré-fixada(a++). A forma pré-fixada atribiu o valor antes da varivael ser usada
+já a pós-fixada atribui depois da varivael ser usada Operadores relacionais
+    Operadores relacionais
+    == -> Igual
+    != -> Diferente
+    > -> Maior que
+    < -> Menor que
+    >= -> Maior ou igual
+    <= -> Menor ou igual
+    Operadores lógicos
+    && -> AND (e)
+    || -> OR (ou)
+    ! -> NOT (não)
+    Operadores de atribução
+    = -> Atribuição simples
+    += -> Atribuição com soma
+    -= -> Atribução com subtração
+    *= -> Atribuição com multiplicação
+    /= -> Atribuição com divisão
+    Operadores diversos
+    sizeof() -> Tamanho da variavel em bytes
+    & -> Endereço da variavel
+    * -> Posição apontada na memoria
+    Fonte: https://www.inf.ufpr.br/roberto/ci067/02_operad.html
+  */
+  // Imprime no console a palavra "Operadores"
+  printf("Operadores\n");
+  return 0;
+}
